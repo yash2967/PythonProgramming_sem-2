d1d1498c70b128231ac4844cff815426eda1b3cd
yash2967 <pansuriyayash19@gmail.com>
2026-01-19 13:25:07 +0530

    Create 25BPE130_13.py

---
+++ 25BPE130_13.py
@@ -0,0 +1,11 @@
+def bytes() :
+    bytes = int(input("Enter bytes : "))
+    kb = bytes/1024
+    mb = kb/1024
+    gb = mb/1024
+
+    print("KB is :",kb)
+    print("MB is :",mb)
+    print("GB is :",gb)
+
+bytes()
