dc62e6855cec2c1bb07d8d62318bed2b50aa64c7
yash2967 <pansuriyayash19@gmail.com>
2026-01-19 13:05:31 +0530

    Create 25BPE130_5.PY

---
+++ 25BPE130_5.PY
@@ -0,0 +1,9 @@
+def all() :
+      a = int(input("Enter a number : "))
+      b = int(input("Enter another number : "))
+      print("addition is :",a+b)
+      print("subtraction is :",a-b)
+      print("multiplication is :",a*b)
+      print("divide is :",a/b)
+
+all()
