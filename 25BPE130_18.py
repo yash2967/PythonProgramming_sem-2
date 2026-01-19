036575cfdbd9e63957e9c7b21588d9c3a4939448
yash2967 <pansuriyayash19@gmail.com>
2026-01-19 13:30:02 +0530

    Create 25BPE130_18.py

---
+++ 25BPE130_18.py
@@ -0,0 +1,11 @@
+def rectangle() :
+    l = int(input("Enter length : "))
+    b = int(input("Enter breadth : "))
+
+    area = l*b
+    perimeter = 2*(l+b)
+
+    print("Area is :",area)
+    print("perimeter is :",perimeter)
+
+rectangle()
