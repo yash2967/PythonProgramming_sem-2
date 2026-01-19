b25517a8fe10858503a6164b4ce52f3153dc7636
yash2967 <pansuriyayash19@gmail.com>
2026-01-19 13:09:41 +0530

    Add function to convert dollars to pounds

---
+++ 25BPE130_10.py
@@ -0,0 +1,8 @@
+def pounds() :
+    dollar = int(input("Enter dollars : "))
+    # 1 dollar = 48 rupees
+    # 1 pound = 70 rupees
+    pound = (70/48)*dollar
+    print("pounds is :",pound)
+
+pounds()
