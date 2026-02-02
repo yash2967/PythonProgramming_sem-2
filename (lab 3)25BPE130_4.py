a2e9ef258aaea1bafb533342734b02bfd9279ef7
yash2967 <pansuriyayash19@gmail.com>
2026-02-02 13:10:41 +0530

    Create (lab 3)25BPE130_4.py

---
+++ (lab 3)25BPE130_4.py
@@ -0,0 +1,9 @@
+def divisible() :
+    a = int(input("Enter a number : "))
+
+    if a%10 == 0:
+        print("Given number is divisible by 10")
+    else:
+        print("Given number is not divisible by 10")
+
+divisible()
