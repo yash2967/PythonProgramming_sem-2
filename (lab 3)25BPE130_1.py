27ff5b07ffae314a9e069c1e9d41e96b3ba40671
yash2967 <pansuriyayash19@gmail.com>
2026-02-02 13:08:58 +0530

    Create (lab 3)25BPE130_1.py

---
+++ (lab 3)25BPE130_1.py
@@ -0,0 +1,12 @@
+def largest() :
+    a = int(input("Enter a number : "))
+    b = int(input("Enter another number : "))
+
+    if a>b:
+        print("a is largest")
+    elif b>a:
+        print("b is largest")
+    else:
+        print("both are equal")
+
+largest()
