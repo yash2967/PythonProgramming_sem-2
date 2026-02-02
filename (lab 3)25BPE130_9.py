1f05c3ee196300ea3e64a71ac3e5a79f72108f6c
yash2967 <pansuriyayash19@gmail.com>
2026-02-02 13:18:28 +0530

    Add function to calculate absolute value of a number

---
+++ (lab 3)25BPE130_9.py
@@ -0,0 +1,9 @@
+def absolute() :
+    a = int(input("Enter a number : "))
+
+    if a >= 0:
+        print("Absolute value is :",a)
+    else:
+        print("Absolute value is :",(-1)*a)
+
+absolute()
