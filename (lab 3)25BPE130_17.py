dc870f6369779415d923b25ac077559a75b9ce6e
yash2967 <pansuriyayash19@gmail.com>
2026-02-02 13:30:12 +0530

    Create (lab 3)25BPE130_17.py

---
+++ (lab 3)25BPE130_17.py
@@ -0,0 +1,9 @@
+def square() :
+    side = int(input("Enter side : "))
+    area = side*side
+    perimeter = 4*side
+
+    print("Area is :",area)
+    print("Perimeter is :",perimeter)
+
+square()
